2ad3d47a5aff89fa390b675564e6bda070c98579
RzCs <168328462+RzCs1@users.noreply.github.com>
2024-11-02 18:16:38 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,34 @@
+// $(document).ready(function() {
+//     $("a").click(function() {
+//         $.ajax({
+//             type: "GET",
+//             url: "window.location.href",
+//             datatype: "xml",
+//             error: function(jqXHR, textStatus, errorThrown) {
+//                 if (jqXHR.status == 404) {
+//                 }
+//             },
+//             success:
+//     )};
+// });
+// });
+
+
+
+
+// function (){
+//     var linkVal
+
+//     document.getElementsByTagName('a').href = linkVal;
+
+//     if (linkVal == ""){
+//     anchor.setAttribute('href', '<../../../../>');}
+// }
+
+// function changeHref1() {
+//     var url = document.querySelector("a");
+//     url.href = 'html-pages\forms-error-others\page-not-found.html';
+//     url.textContent = "Hello, Welcome to Tutorialspoint";
+//  }
+
+
